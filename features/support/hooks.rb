5146811de26c878b123edb4204352ed98b034272
Argemiro <argemiro.pcosta@gmail.com>
2020-07-17 11:29:03 -0300

    - Salvando screenshots dos erros;

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,7 +2,21 @@ Before do
   $driver.start_driver
 end
 
-After do
-  sleep 3
+After do |scenario|
+  if scenario.failed?
+    if !File.directory?("reports/screenshots")
+      FileUtils.mkdir_p("reports/screenshots")
+    end
+
+    time_stamp = Time.now.strftime("%d-%m-%Y_%H:%M:%S")
+    screenshot_name = time_stamp + ".png"
+    screenshot_file = File.join("reports/screenshots", screenshot_name)
+    $driver.screenshot(screenshot_file)
+    attach "#{screenshot_file}", "image/png"
+  end
   $driver.driver_quit
+end
+
+AfterConfiguration do
+  FileUtils.rm_r("reports/screenshots") if File.directory?("reports/screenshots")
 end

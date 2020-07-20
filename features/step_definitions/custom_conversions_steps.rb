7f1cd480679eb6c0866092f137aed2d9fa5c467f
Argemiro <argemiro.pcosta@gmail.com>
2020-07-20 12:13:02 -0300

    - Melhorias no código dos cenários de custom_conversions.

--- features/step_definitions/custom_conversions_steps.rb
+++ features/step_definitions/custom_conversions_steps.rb
@@ -14,20 +14,12 @@ Então('eu digito {string} como nome da unidade') do |unit_name|
   find_element(id: "edit_custom_conversion_unit_dtls_name").send_keys(unit_name)
 end
 
-Então('eu digito {string} como símbolo da unidade') do |symbol_name|
-  find_element(id: "edit_custom_conversion_unit_dtls_symbol").send_keys(symbol_name)
-end
-
 Então('eu digito {string} como valor da unidade') do |unit_value|
   find_element(id: "edit_custom_conversion_unit_dtls_value").send_keys(unit_value)
 end
 
 Então('eu toco no ícone de check na tela de conversões customizadas') do
   find_element(id: "action_confirm_custom_unit").click
-end
-
-Quando('eu toco no botão Ok na tela de conversões customizadas') do
-  find_element(id: "btn_custom_conversion_details_ok").click
 end
 
 Então('eu verifico {string} foi adicionado a lista de conversões customizadas') do |conversion_name|
